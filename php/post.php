10be304dce6984cf5473aa8114e1316a89ea6b05
evanpaul22 <evanpaul22@gmail.com>
2016-11-14 20:59:49 -0800

    More styling, fewer bugs (hopefully)

--- php/post.php
+++ php/post.php
@@ -2,7 +2,7 @@
 
 include 'data.php';
 
-if(isset($_POST['id']) && $_POST['classes'] && $_POST['eduFlag']){
+if(isset($_POST['id'])){
     $json = ['id' => $_POST['id'], 'classes' => $_POST['classes'], 'eduFlag' => $_POST['eduFlag']];
     $json_string = json_encode($json);
     save_session($json_string);
